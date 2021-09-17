cd44964b22873094b7bcbb3e2aa0dd54ccc4db1e
Sebastian Widmer <sebastian.widmer@vshn.net>
2021-09-17 11:29:38 +0200

    Converge hiera rendering and git

--- modules/lb-vshn-hiera/git.tf
+++
@@ -1,9 +0,0 @@
-module "git" {
-  source = "./modules/git"
-
-  count = local.lb_count > 0 ? 1 : 0
-
-  hieradata_repo_user = var.hieradata_repo_user
-  cluster_id          = var.cluster_id
-  content             = local.content
-}
--- modules/lb-vshn-hiera/modules/git/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "hieradata_mr_url" {
-  value = data.local_file.hieradata_mr_url.content
-}
