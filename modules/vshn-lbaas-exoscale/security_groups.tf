e2e5ba490778068ed523576559e4f9c0b5a59f3c
Simon Gerber <simon.gerber@vshn.ch>
2022-10-05 13:40:12 +0200

    vshn-lbaas-exoscale: Only open port 6443 (K8s API) for TCP traffic
    This fixes an oversight in #30 where we refactored the security group
    rules and mistakenly included the Kubernetes API port in the ports to
    open for UDP traffic.

--- modules/vshn-lbaas-exoscale/security_groups.tf
+++ modules/vshn-lbaas-exoscale/security_groups.tf
@@ -1,10 +1,12 @@
 locals {
   # security group rules to create for TCP and UDP over IPv4 and IPv6
-  open_ports = {
-    "Kubernetes API"           = "6443",
+  open_ports_udp = {
     "Ingress controller HTTP"  = "80",
     "Ingress controller HTTPS" = "443",
   }
+  open_ports_tcp = merge(local.open_ports_udp, {
+    "Kubernetes API" = "6443",
+  })
 }
 
 resource "exoscale_security_group" "load_balancers" {
@@ -13,7 +15,7 @@ resource "exoscale_security_group" "load_balancers" {
 }
 
 resource "exoscale_security_group_rule" "load_balancers_tcp4" {
-  for_each = local.open_ports
+  for_each = local.open_ports_tcp
 
   security_group_id = exoscale_security_group.load_balancers.id
 
@@ -26,7 +28,7 @@ resource "exoscale_security_group_rule" "load_balancers_tcp4" {
 }
 
 resource "exoscale_security_group_rule" "load_balancers_tcp6" {
-  for_each = local.open_ports
+  for_each = local.open_ports_tcp
 
   security_group_id = exoscale_security_group.load_balancers.id
 
@@ -39,7 +41,7 @@ resource "exoscale_security_group_rule" "load_balancers_tcp6" {
 }
 
 resource "exoscale_security_group_rule" "load_balancers_udp4" {
-  for_each = local.open_ports
+  for_each = local.open_ports_udp
 
   security_group_id = exoscale_security_group.load_balancers.id
 
@@ -52,7 +54,7 @@ resource "exoscale_security_group_rule" "load_balancers_udp4" {
 }
 
 resource "exoscale_security_group_rule" "load_balancers_udp6" {
-  for_each = local.open_ports
+  for_each = local.open_ports_udp
 
   security_group_id = exoscale_security_group.load_balancers.id
 
